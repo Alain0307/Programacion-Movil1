69a40c12dd89b92db22c8e5081086066259025a5
Alain Cesar <113079684+Alain0307@users.noreply.github.com>
2025-09-01 17:56:10 -0600

    Programa 2

---
+++ holamundo/HolaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author elga0
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(args[0]);
+        int temp = numero%2;
+        
+        if(temp == 0){
+            System.out.println("el " +numero+ " es numero par");
+        }else{
+            System.out.println("el " +numero+ " es numero impar");
+        }
+    }
+}
