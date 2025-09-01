9785c6ef61f2755c14f2f57af5688858fb629c02
Alain Cesar <113079684+Alain0307@users.noreply.github.com>
2025-09-01 17:34:28 -0600

    Programa 1

---
+++ prueba/Prueba.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author elga0
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        HolaMundo hm = new HolaMundo();
+        hm.nombre = "Omar";
+        hm.saludar("Ana");
+    }
+    
+    public static class HolaMundo{
+        String nombre = "John Doe";
+        
+        public void saludar(String nombre){
+            this.nombre = nombre;
+            System.out.println("Hola Mundo" + nombre);
+        }
+    }
+}
